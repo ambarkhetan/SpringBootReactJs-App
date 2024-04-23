a928c19b917ee4a308cd64a9e054e3a23db1b7f4
ambarkhetan <amberkhetan.dev@gmail.com>
2024-04-23 14:53:59 +0530

    SpringBoot With. React App

---
+++ react-frontend/src/components/root/fragments/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Footer = (props) => {
+  return (
+    <footer className={props.class}>
+      &copy; Ambar 2024. All rights reserved.
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ react-frontend/src/components/root/fragments/footer/FooterCover.jsx
@@ -0,0 +1,12 @@
+// import React from "react";
+// import styles from "../../../../css/Footer.module.css";
+
+// export const FooterCover = () => {
+//   return (
+//     <footer className={styles.footer_cover}>
+//       &copy; Ambar 2024. All rights reserved.
+//     </footer>
+//   );
+// };
+
+// export default FooterCover;
